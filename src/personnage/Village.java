88876ae5adc646f60406f1fc738b9817c33d054f
Lucas Lefebvre <lfl5405a@ensfsi.onmicrosoft.com>
2024-03-01 08:49:48 +0100

    ajout de la methode afficherVillageois de village

--- src/personnage/Village.java
+++ src/personnage/Village.java
@@ -20,27 +20,37 @@ public class Village {
 	public String getNom() {
 		return nom;
 	}
-	
+
 	public void ajouterHabitant(Gaulois gaulois) {
 		this.villageois[nbVillageois] = gaulois;
-		nbVillageois ++;
-		
+		nbVillageois++;
+
 	}
-	
+
 	public Gaulois trouverHabitant(int numVillageois) {
 		return this.villageois[numVillageois];
 	}
-	
+
+	public void afficherVillageois() {
+		System.out.println("Dans le village du chef " + this.chef.getNom() + " vivent les légendaires gaulois :");
+
+		for (int i = 0; i < this.nbVillageois; i++) {
+			System.out.println("- " + this.villageois[i].getNom());
+		}
+
+	}
+
 	public static void main(String[] args) {
-		Village village = new Village("Village des Irréductibles",30);
+		Village village = new Village("Village des Irréductibles", 30);
 //		Gaulois gaulois = village.trouverHabitant(30);
 //		exeption index out of bound -> les indice vont de 0 a 29
 		Chef abraracourcix = new Chef("Abraracourcix", 8, village);
+		village.setChef(abraracourcix);
 		Gaulois asterix = new Gaulois("Asterix", 8);
 		village.ajouterHabitant(asterix);
 //		Gaulois gaulois = village.trouverHabitant(1);
 //		System.out.println(gaulois);
 //		l'indice de astezrix est 0 malgrés le fait que ce soit le premier villageois, le reste du tableaux est compose de Gaulois null
-		
+		village.afficherVillageois();
 	}
 }
